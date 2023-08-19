ee6185e6ad90fbee7946d51ee5d18ce58b2f03a1
William Martinez <68308482+wmb81321@users.noreply.github.com>
2023-08-19 00:25:45 -0500

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -5,17 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/// @custom:security-contact wmb81321@gmail.com
 contract TKNCALI is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC20("TKNCALI", "ETHCALI") {
         _mint(msg.sender, 1000 * 10 ** decimals());
-
-        // Asignar el rol de administrador al creador del contrato.
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-
-        // Asignar el rol de minter al creador del contrato.
-        _setupRole(MINTER_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
